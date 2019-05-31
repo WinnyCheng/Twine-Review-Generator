cf34ecb61eec4bdb8b92dc098a665b6c58b8e2ea
Jose Li <jaliquiel@gmail.com>
2019-05-31 17:39:22 -0400

    Start refactoring to make graph be able to read cycle in twine games.

--- Bot1/Graph.js
+++ Bot1/Graph.js
@@ -1,3 +1,9 @@
+/* Todo:
+- Finish implementing Text array
+- Implement function that translates from text into ID
+
+- Adjust the make graph algorithm 
+*/
 
 $.ajaxSetup({
     async: false
@@ -9,13 +15,19 @@ $.ajaxSetup({
 class Graph {
     constructor(){
         this.V = 0;
-        this.E = new Map();
+        this.E = new Map(); // Vertex number -> children
+        this.Text = [];
         this.M = new Map();
     }
-    addVertex(v){
+
+    // Give the text paragraph of a section, and make a vertex in the graph
+    // updates Edges and Marked
+    addVertex(text){
         this.V = this.V + 1;
-        this.E.set(v, []);
-        this.M.set(v, false);
+        // todo add function to get children
+        // var children = getChildren(text);
+        this.E.set(text, []);
+        this.M.set(text, false);
     }
     addEdge(v, w){
         this.E.get(v).push(w);
@@ -23,7 +35,20 @@ class Graph {
     mark(v){
         this.M.set(v, true);
     }
+
+    addText(text){
+        this.Text.push(text);
+    }
+
+    // iterate through the Text array and return the index where that matches the input text
+    // if not found returns -1
+    getIndex(text){
+        return this.Text.indexOf(text);
+    }
+
+    // todo edit print Graph to accomodate for E being an array now
     printGraph(){
+        console.log("I will start printing the graph")
         var keys = this.E.keys();
         for(var i of keys){
             var val = this.E.get(i);
@@ -36,58 +61,83 @@ class Graph {
     }
 }
 
+// todo this should be a function of the node ***
+// INPUT: text from novel node
+// RETURN: Array with the children text of the current node
+// it also clicks and returns in order to stay in the same node
+function getChildren(graph, text){
+    // do something
+
+    return [];
+}
+
 var url = "http://localhost:3000/links";
 
 var g = new Graph();
-g.addVertex("Beginning");
+// g.addVertex("Beginning");
 
-play(" ", "Beginning");
+// play(" ", "Beginning");
 
-function play(pre, v) {
-    $.getJSON(url, function (data) {
-        var links = data.links;
-        var numLinks = data.links.length;
-
-        //end of game
-        if (numLinks === 0) {
-            g.mark(v);
-        }
-        else if(g.M.get(v)){
-            //do nothing
-        }
-        //next stage of game, choose option, move on
-        else {
-            for(let i = 0; i < numLinks; i++) {
-                if(i === numLinks-1){
-                    g.mark(v);
-                }
-                // setTimeout(function(){
-                    //check if vertex of link exist
-                    var keys = g.E.keys();
-                    var newLink = true;
-                    for (let k of keys) {
-                        //add edge
-                        if (k === links[i].text) {
-                            g.addEdge(v, k);
-                            newLink = false;
-                            break;
-                        }
-                    }
-                    if (newLink) {
-                        //create vertex and add edge
-                        g.addVertex(links[i].text);
-                        g.addEdge(v, links[i].text);
-                    }
-                    $.get("http://localhost:3000/click/" + i, function () {
-                        // console.log(links[i].text);
-                        play(links[0].text, links[i].text);
-                        $.get("http://localhost:3000/undo");
-                        console.log("Play");
-                    });
-                // }, 2000);
-            }
-        }
-    })
-}
+//
+$.getJSON("http://localhost:3000/text", function (data) {
+    // do something
+    // console.log(data['text']);
+    g.addVertex(data['text']);
+});
 
 g.printGraph();
+
+
+
+
+
+
+
+// function play(pre, v) {
+//     $.getJSON("http://localhost:3000/links", function (data) {
+//         var links = data.links;
+//         var numLinks = data.links.length;
+//
+//         //end of game
+//         if (numLinks === 0) {
+//             g.mark(v);
+//         }
+//         else if(g.M.get(v)){
+//             //do nothing
+//         }
+//         //next stage of game, choose option, move on
+//         else {
+//             for(let i = 0; i < numLinks; i++) {
+//                 if(i === numLinks-1){
+//                     g.mark(v);
+//                 }
+//                 //check if vertex of link exist
+//                 var keys = g.E.keys();
+//                 var newLink = true;
+//                 for (let k of keys) {
+//                     //add edge
+//                     if (k === links[i].text) {
+//                         g.addEdge(v, k);
+//                         newLink = false;
+//                         break;
+//                     }
+//                 }
+//                 if (newLink) {
+//                     //create vertex and add edge
+//                     g.addVertex(links[i].text);
+//                     g.addEdge(v, links[i].text);
+//                 }
+//
+//
+//                 $.get("http://localhost:3000/click/" + i, function () {
+//                     // console.log(links[i].text);
+//                     play(links[0].text, links[i].text);
+//                     $.get("http://localhost:3000/undo");
+//                     console.log("Play");
+//                 });
+//             }
+//         }
+//     })
+// }
+//
+// g.printGraph();
