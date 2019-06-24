ced283d6ebab7837a5d22430c8a9298cf9888a80
Winny Cheng <ccheng2@wpi.edu>
2019-06-24 16:59:37 -0400

    Clean up some comments and set default start vertex name ID with variable

--- Bot1/Graph.js
+++ Bot1/Graph.js
@@ -1,42 +1,25 @@
-
-/* Todo:
-- Finish implementing Text array
-- Implement function that translates from text into ID
-- Adjust the print graph function
-- Test with different novels
-Bugs:
-- It doesn't go into recursion
-- Debug why D is not being added into the shit
-*/
-
+//sets all javascript code to be synchronous
 $.ajaxSetup({
     async: false
 });
 
 var a = [];
 
-// creates graph and then multiple play through will be from graph
-
 // graph implementation
 class Graph {
     constructor(){
         this.V = 0;
-        this.E = new Map(); // Vertex nameID -> obj [string text, int vertexNum, array of nameID children]
-        // children = object{ text , clickIndex, nameID }
-        this.M = new Map();
-        // this.containsCyclingLink = false;
+        this.E = new Map(); // Vertex nameID -> obj [string text, int vertexNum, array of nameID children, array of link type]
+        this.M = new Map(); // Vertex nameID -> boolean
     }
-
-    // Give the text paragraph of a section, and make a vertex in the graph
-    // updates Edges and Marked
+    //Adds a new vertex to the graph with given ID name
     addVertex(nameID) {
-        this.V = this.V + 1;
-        let children = getChildren();
-        let edgeTo = [];
-        let type = [];
+        this.V = this.V + 1; //counter for total number of vertices in the graph
+        let children = getChildren(); //get all the links and link types, returns an array of objects
+        let edgeTo = []; let type = [];
         for(let c of children){
-            edgeTo.push(c['nameID']);
-            type.push(c['linkType']);
+            edgeTo.push(c['nameID']); //list of children name IDs
+            type.push(c['linkType']); //list of link types matching children's name IDs
         }
         let ver = {
             numID: this.V,
@@ -44,24 +27,18 @@ class Graph {
             Children: edgeTo,
             linkType: type
         };
-        // console.log(children);
         this.E.set(nameID, ver);
-        this.M.set(nameID, false);
+        this.M.set(nameID, false); //vertex is not marked
     }
-
+    //replaces the name ID of a vertex
     replaceVertex(oldName, newName){
         this.E.set(newName, this.E.get(oldName));
         this.E.delete(oldName);
         this.V = this.V - 1;
     }
-
-    // addEdge(v, w){
-    //     this.E.get(v).push(w);
-    // }
     mark(v) {
         this.M.set(v, true);
     }
-
     // grabs passage text from current passage
     getText(){
         let passageText = "";
@@ -70,63 +47,8 @@ class Graph {
         });
         return passageText.replace(/↶\n|↷\n/g, "");
     }
-
-    // todo this should be a function of the node ***
-    // INPUT: passage nameID from a specific passage in the novel
-    // RETURN: Array of objects. Each object represents a child of the parent.
-    // Each child object contains text, nameID and clickIndex.
-    // getChildren(){
-    //     let children = [];
-    //     let links = [];
-    //     // var containsCyclingLink = false;
-    //
-    //     $.getJSON("http://localhost:3000/links", function (data) {
-    //         links = data['links'];
-    //         let numLinks = data['links'].length;
-    //
-    //         // array containing all the cycling links in the html vertix
-    //         var cyclingLinks = isCyc();
-    //         // array containing all the children IDs from the current passage
-    //         var childrenIDs = getChildrenIDs();
-    //
-    //         for(let i = 0; i < numLinks; i++) {
-    //             // check if link is a cycling link, if yes don't click on it
-    //             // else continue normal logic and click on it
-    //             if (cyclingLinks.includes(links[i]['text'])){
-    //                 console.log(links[i]['text']);
-    //                 // containsCyclingLink = true;
-    //                 // this.setCyclingLink();
-    //                 // dont do anything
-    //             }else{
-    //                 // this is not a cycling link
-    //                 $.get("http://localhost:3000/click/" + i, function () {
-    //                     $.get("http://localhost:3000/text", function (text) {
-    //                         // text is text response
-    //                         // console.log("the text is:");
-    //                         // console.log(text['text']);
-    //                         let child = {
-    //                             text : text['text'].replace(/↶\n|↷\n/g, ""),
-    //                             clickIndex: i,
-    //                             nameID: childrenIDs[i] // todo how to know where to index
-    //                         };
-    //                         children.push(child);   // debug here
-    //
-    //                     });
-    //                     $.get("http://localhost:3000/undo");
-    //                 });
-    //             }
-    //         }
-    //     });
-    //     // console.log("my children are: ")
-    //     // console.log(children);
-    //     // if (containsCyclingLink){
-    //     //     this.containsCyclingLink = true;
-    //     // }
-    //     return children;
-    // }
-
-    // todo edit print Graph to accomodate for E being an array now
-    // todo print Marked array
+    // todo merge printGraph implementation from Vizjs branch
+    //print all vertices and connected edges
     printGraph(){
         console.log("I will start printing the graph");
         var keys = this.E.keys();
@@ -139,25 +61,23 @@ class Graph {
             }
             console.log(this.E.get(i)['numID'] + " -> " + str);
             // console.log(i + " -> " + str);
-            // console.log(i);
             a.push({data: { id: i, name: i }});
             a.push({data: { id: str, name: str}});
             a.push({data: { source: i, target: str} });
         }
         console.log("my Vertex are: " + g.V);
         console.log("length of keys are: " + g.E.size);
-        // console.log(g.M);
     }
 }
 
 //check duplicates with beginning vertex
 function matchStart(nameID){
-    //if nameID is start return
-    if(nameID === "Start")
+    //if nameID is default return or default is no longer in graph
+    if(nameID === defaultVer || !g.E.has(defaultVer))
         return false;
 
-    //grab text and children of start vertex
-    let sObj = g.E.get("Start");
+    //grab text and children of beginning vertex
+    let sObj = g.E.get(defaultVer);
     let sText = sObj['Text'];
     let sChildren = sObj['Children'];
     //grab text and children of given vertex
@@ -170,7 +90,7 @@ function matchStart(nameID){
 
     return tMatch && cMatch;
 }
-
+//checks if the given array of children are the same
 function isChildrenEqual(childrenA, childrenB){
     for(var i = 0; i < childrenA.length || i < childrenB.length; i++){
         if(childrenA[i] !== childrenB[i])
@@ -179,17 +99,12 @@ function isChildrenEqual(childrenA, childrenB){
     return true;
 }
 
-let hasStart = true;
 // INPUT: string nameID which represents the passage-name of each passage.
-// RETURNS:
+// Traverses through the whole twine game and creates a graph representation
 function play(nameID) {
     // console.log('starting to play');
     let children = g.E.get(nameID)['Children'];
     let linkType = g.E.get(nameID)['linkType'];
-
-    // Iterate through each of the links/children of the current vertex/text
-    // $.getJSON("http://localhost:3000/links", function (data) {
-        // let links = data['links'];
 
         for(let i = 0; i < children.length; i++) {
             // check if children is in hashmap
@@ -201,38 +116,17 @@ function play(nameID) {
                 // add child to graph
                 $.get("http://localhost:3000/click/" + i, function () {
                     g.addVertex(children[i]);
-                    if(hasStart) {
-                        if(matchStart(children[i])) {
-                            g.replaceVertex("Start", children[i])
-                            hasStart = false;
-                        }
-                    }
+                    //replace beginning default if matches
+                    if(matchStart(children[i]))
+                        g.replaceVertex(defaultVer, children[i]);
                     play(children[i]);
                     $.get("http://localhost:3000/undo");
                 });
             }
         }
-    // });
+
     g.mark(nameID);
 }
-
-// todo change the name of the function
-//RETURN: an array with the names of the links that are cycling links
-// function isCyc(){
-//     var hasCyc = [];
-//     $.getJSON("http://localhost:3000/html", function(data){
-//         var str = data.html;
-//         while(str.length > 0 && str.includes("cycling-link")) {
-//             str = str.substring(str.indexOf("cycling-link"));
-//             var link = str.split(">", 3)[2];
-//             link = link.substring(0, link.indexOf("<"));
-//
-//             hasCyc.push(link);
-//             str = str.substring(str.indexOf(link));
-//         }
-//     });
-//     return hasCyc;
-// }
 
 // RETURN: an array of objects containing the children IDs of the current passage
 // each object is in the format [nameID: string, linkType: string]
@@ -290,15 +184,8 @@ var url = "http://localhost:3000/links";
 var g = new Graph();
 $.getJSON("http://localhost:3000/reset");
 
-// Add starting vertex to graph
-// $.getJSON("http://localhost:3000/text", function (data) {
-//     // do something
-//     // console.log(data['text']);
-//     g.addVertex(data['text'].replace(/↶\n|↷\n/g, ""));
-//     play(data['text'].replace(/↶\n|↷\n/g, ""));
-//     // setTimeout(g.printGraph,10000);
-// });
-
-g.addVertex("Start");
-play("Start");
+// Add beginning vertex to graph
+let defaultVer = "Begin";
+g.addVertex(defaultVer); //add initial vertex to graph
+play(defaultVer);
 g.printGraph();
