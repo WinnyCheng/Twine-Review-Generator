4c53ed9649e511228f77a10b517a42711717c222
Winny Cheng <ccheng2@wpi.edu>
2019-07-05 15:05:00 -0400

    Cleaning up comments

--- Bot1/script.js
+++ Bot1/script.js
@@ -27,69 +27,6 @@ function play() {
     })
 }
 
-// var numGames = 20;
-// var millsec = 3000;
-//
-// //multiply play through with delay on a game play
-// function checkReset(){
-//     $.getJSON(url, function(data){
-//         //not finished resetting
-//         if(data.links.length === 0){
-//             checkReset();
-//         }
-//         //reset finished, play twine
-//         else{
-//             play("");
-//         }
-//     })
-// }
-//
-//initial play
-// play();
-//
-// //make sure game is rested before playing twine again
-// for(let i = 1; i < numGames; i++){
-//     setTimeout(function(){
-//         //reset twine game
-//         $.get("http://localhost:3000/reset", function(){
-//             checkReset();
-//         });
-//     }, millsec*i);
-// }
-//
-// function checkReset2(){
-//     $.getJSON(url, function(data){
-//         //not finished resetting
-//         if(data.links.length === 0){
-//             checkReset2();
-//         }
-//         //reset finished, print sum average
-//         else{
-//             var sum = numPlays.reduce(getSum);
-//             var average = sum / numPlays.length;
-//             var max = Math.max(...numPlays);
-//             var min = Math.min(...numPlays);
-//
-//             console.log(sum);
-//             console.log(average);
-//             console.log(max);
-//             console.log(min);
-//
-//             document.getElementById("average").innerText = "Average: " + average;
-//             document.getElementById("max").innerText = "Max: " + max;
-//             document.getElementById("min").innerText = "Min: " + min;
-//         }
-//     })
-// }
-//
-// //last reset
-// setTimeout(function(){
-//     $.get("http://localhost:3000/reset", function(){
-//         checkReset2();
-//     });
-// }, millsec*numGames);
-//
-
 //setting asynchronous to false
 $.ajaxSetup({
     async: false
