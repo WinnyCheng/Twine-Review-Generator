566122ae866f2fe3e2cb9e72fc2bfb8caec4b33e
Winny Cheng <ccheng2@wpi.edu>
2019-07-17 15:44:19 -0400

    trimed strings, fixed a small bug

--- Graph/Graph.js
+++ Graph/Graph.js
@@ -45,7 +45,8 @@ class Graph {
             var val = this.E.get(i)['LinksPass'];
             var str = "";
             for(var j of val){
-                str += this.E.get(j)['PID'] + " ";
+                if(!str.includes(this.E.get(j)['PID']))
+                    str += this.E.get(j)['PID'] + " ";
                 // str += j + " ";
             }
 
@@ -53,8 +54,8 @@ class Graph {
             children.pop();
 
             if(str !== "" && children.length !== 0) {
-                // console.log(this.E.get(i)['PID'] + " -> " + str);
-                // console.log(this.E.get(i)['PID'] + " -> " + children);
+                console.log(this.E.get(i)['PID'] + " -> " + str);
+                console.log(this.E.get(i)['PID'] + " -> " + children);
                 // console.log(children.length);
                 for(let k = 0; k < children.length; k++) {
                     graph = graph.insert(graph.length - 2, " " + this.E.get(i)['PID'] + " -> " + children[k]);
@@ -77,77 +78,66 @@ class Graph {
     singlePath(){
         //keep track of which vertices are visited and marked so it doesn't
         // go back to a path it already gone through
-        var seen = new Map();
-        var mark = new Map();
+        var seen = new Map(); var mark = new Map();
         var story = "";
-        var twoLinks = 0;
-        var mulitpleLinks = 0;
-        var start = "";
-        //grab first vertex, vertex with ID number 1
-        for(let k of this.E.keys()){
-            if(this.E.get(k)['numID'] === 1){
-                start = k;
-                break;
-            }
-        }
+        var twoLinks = 0; var mulitpleLinks = 0;
+        var start = this.getFirstPID();
+        var path = [];
 
-        // console.log("start: " + start);
         var vertex = this.E.get(start);
-        var child = vertex['Children'];
-
-        //save text
-        story += vertex['Text'];
-
+        path.push(start);
+        seen.set(vertex['PID'], true);
+        var child = vertex['LinksPass'];
+        story += vertex['Text']; //save text
         var childrenEmpty = child.length === 0;
-        //var returnedToStart = false;
-
+        var backToBeginning = false;
         //go down random path till "End" of story
         //End condition
         // 1. No more links aka children array is empty
-        // 2. It returns to starting vertex aka vertex number ID 1
-        while(!childrenEmpty){
+        // 2. It returns to starting vertex aka vertex number ID 1 and is a restart/start over
+        while(!childrenEmpty && !backToBeginning){
             //check number of links of current vertex
-            if(child.length === 2)
-                twoLinks++;
-            else if(child.length > 2)
-                mulitpleLinks++;
+            if(child.length === 2) twoLinks++;
+            else if(child.length > 2) mulitpleLinks++;
 
-            do {
-                //random edge to next vertex
+            do { //random edge to next vertex
                 var index = Math.floor(Math.random() * child.length);
-            }
-            while(mark.has(this.E.get(child[index])['numID'])); //random index that is unmarked
+            } while(mark.has(this.E.get(child[index])['PID'])); //random index that is unmarked
 
             //check if all seen then mark
             let marked = true;
-            for(let c of child){
+            for(let c of child)
                 marked = marked && mark.has(c);
-            }
-            if(marked)
-                mark.set(vertex['numID'], true);
+            mark.set(vertex['PID'], marked);
 
             var current = child[index];
-            ///test
-                //console.log(current);
-            ///test
+            var linkName = vertex['LinksName'][index];
             vertex = this.E.get(current);
-            if(!seen.has(vertex['numID']))
+            path.push(current);
+            if(!seen.has(vertex['PID']))
                 story += vertex['Text']; //save text
-            seen.set(vertex['numID'], true);
-            child = vertex['Children'];
-
+            seen.set(vertex['PID'], true);
+            child = vertex['LinksPass'];
             if(child.length === 1)
-                mark.set(vertex['numID'], true);
-
+                mark.set(vertex['PID'], true);
             childrenEmpty = child.length === 0;
-            // returnedToStart = current === start;
+            backToBeginning = current === start &&
+                (linkName.toLowerCase() === "restart" ||
+                linkName.toLowerCase() === "start over")
         }
-
+        console.log(path);
         return {
             text: story,
             twoLinks: twoLinks,
             multiLinks: mulitpleLinks
         };
+    }
+    //returns the key of vertex with pid 1
+    getFirstPID(){
+        for(let key of this.E.keys()){
+            //default start passage = passage with pid 1
+            if(this.E.get(key)['PID'] === "1") return key;
+        }
     }
 }
 
@@ -172,7 +162,7 @@ function setText(vertex, g){
         let links = data['links'];
 
         for(let i = 0; i < links.length/2; i++){ //******divide by 2 cuz repeats********
-            var name = links[i]['text'];
+            var name = links[i]['text'].trim();
             var index = linkNames.indexOf(name);
             vertex = linkPass[index];
             if(linkNames.includes(name) && !g.visited.has(vertex)) {
@@ -200,7 +190,7 @@ function parseSource(){
                 var links = getLinks(text);
                 var passData = {
                     pid: attr.pid.nodeValue,
-                    name: attr.name.nodeValue,
+                    name: trim(attr.name.nodeValue),
                     text: text,
                     linksName: links[0],
                     linksPass: links[1]
@@ -219,7 +209,9 @@ function getLinks(text){
     var str = text;
 
     while(str.length > 0 && str.includes("[[") && str.includes("]]")){
-        links.push(str.substring(str.indexOf("[[")+2, str.indexOf("]]")));
+        var alink = str.substring(str.indexOf("[[")+2, str.indexOf("]]"));
+        if(!links.includes(alink))
+            links.push(alink);
         str = str.substring(str.indexOf("]]")+2);
     }
 
@@ -231,49 +223,51 @@ function getLinks(text){
     for(let l of links){
         if(l.includes("|")){
             index = l.indexOf("|");
-            linksName.push(l.substring(0, index));
-            linksPass.push(l.substring(index+1));
+            linksName.push(trim(l.substring(0, index)));
+            linksPass.push(trim(l.substring(index+1)));
         }
         else if(l.includes("->")){
             index = l.lastIndexOf("->");
-            linksName.push(l.substring(0, index));
-            linksPass.push(l.substring(index+2));
+            linksName.push(trim(l.substring(0, index)));
+            linksPass.push(trim(l.substring(index+2)));
         }
         else if(l.includes("<-")){
             index = l.lastIndexOf("<-");
-            linksPass.push(l.substring(0, index));
-            linksName.push(l.substring(index+2));
+            linksPass.push(trim(l.substring(0, index)));
+            linksName.push(trim(l.substring(index+2)));
         }
         else{
-            linksName.push(l);
-            linksPass.push(l);
+            linksName.push(trim(l));
+            linksPass.push(trim(l));
         }
     }
 
     return [linksName, linksPass];
 }
+//trims extra spacings
+function trim(str){
+    var wordsArray = str.split(" ");
+    str = "";
+    for(var s of wordsArray) {
+        if(s.length > 0)
+            str += s.trim() + " ";
+    }
+    return str.trim();
+}
 
 //return the created graph of twine game
 function createGraph(){
     var g = new Graph(parseSource());
-
     $.getJSON(url + "reset", function(){ //reset game
         //set text from /text route to vertices
-        for(let key of g.E.keys()){
-            if(g.E.get(key)['PID'] === "1") { //default start passage = passage with pid 1
-                getText(key, g);
-                setText(key, g);
-                break;
-            }
-        }
-        console.log("Counter: " + counter);
+        getText(g.getFirstPID(), g);
+        setText(g.getFirstPID(), g);
     });
-
     return g;
 }
 
 var G = createGraph();
-console.log(G.getStory());
+// console.log(G.getStory());
 G.printGraph();
 
 // draw the graph on page
