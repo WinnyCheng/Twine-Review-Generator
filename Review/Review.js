174723945b806568447cdbb1bf64674ca8adf9c4
Winny Cheng <ccheng2@wpi.edu>
2019-08-07 17:51:05 -0400

    all metrics

--- Review/Review.js
+++ Review/Review.js
@@ -6,10 +6,11 @@ var vertices = g.getNumVer(),
     edges = g.getNumEdge(),
     time = gameplay(g, 100),
     sentiment = getSentiAna(g),
-    encoderStr = getEncoder(g);
+    encoderStr = getEncoder(),
+    readinglvl = storyReadability(g);
 
 function checkFlag(){
-    encoderStr = getEncoder(g);
+    encoderStr = getEncoder();
 
     if(encoderStr.length === 0)
         setTimeout(checkFlag, 1000);
@@ -25,6 +26,7 @@ function printAll(){
         "\nSentimental Analysis: Negative: " + sentiment['negative']['score'] +
         " Positive: " + sentiment['positive']['score'] +
         " Score: " + sentiment['score'] +
+        "\nReadability: Grade1: " + readinglvl[0] + " Grade2: " + readinglvl[1] +
         "\nSimilarity: ";
     for(let val of encoderStr){
         str += "\n" + val;
@@ -35,3 +37,5 @@ function printAll(){
 
 checkFlag();
 g.printGraph();
+
+
